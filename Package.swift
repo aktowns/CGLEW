af058103834e7fccf826c6efdcb7948fee2389c2
Ashley Towns <mail@ashleytowns.id.au>
2016-01-10 21:59:05 +1100

    ..

--- Package.swift
+++ Package.swift
@@ -1,5 +1,8 @@
 import PackageDescription
 
 let package = Package(
-  name: "CGLEW"
+  name: "CGLEW",
+  dependencies: [
+    .Package(url: "https://github.com/aktowns/CGL.git", Version(1,0,0))
+  ]
 )
